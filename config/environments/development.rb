4471fdf8e3c2eb77a464cf215333cabdc891cc1b
Sandi Lin <sandislin@gmail.com>
2013-03-18 08:43:29 -0700

    Fixed ImageMagick on localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Omrails::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   #Fix paperclip's imagemagick identify issue
-Paperclip.options[:command_path] = "/usr/local/bin/"
+Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 
 end
 
